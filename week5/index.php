75bc84b75314a371874b5f4ca15877449ddb45ea
mespsar <surya.sigh@gmail.com>
2020-06-14 12:38:05 +0530

    first commit

---
+++ week5/index.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>suryapal singh</title>
+</head>
+<body>
+<h1>suryapal singh</h1>
+<p>
+<?php	
+echo "The SHA256 hash of suryapal singh is";
+print hash('sha256', 'suryapal singh');
+?>	
+</p>
+<pre>ASCII ART:
+
+    ***********
+    **       
+    **
+    ***********
+             **
+             **
+    ***********
+</pre>
+<a href="check.php">Click here to check the error setting</a>
+<br/>
+<a href="fail.php">Click here to cause a traceback</a>
+</body>
